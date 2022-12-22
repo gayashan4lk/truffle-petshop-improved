49f9a186bccb721ba5f228c730918077f8fcc4ec
Gayashan Wagachchi <gayashan4lk@gmail.com>
2022-12-22 06:54:42 +0530

    add event to smart contract

--- truffle/contracts/Adoption.sol
+++ truffle/contracts/Adoption.sol
@@ -4,10 +4,13 @@ pragma solidity >=0.4.22 <0.9.0;
 contract Adoption {
   address[16] public adopters;
 
+  event petAdopted(uint petid);
+
   // Adopting a pet
   function adopt(uint petId) public returns (uint){
     require(petId >= 0 && petId <=15);
     adopters[petId] = msg.sender;
+    emit petAdopted(petId);
     return petId;
   }
 
